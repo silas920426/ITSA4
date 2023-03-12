4219785ca259acd24147e73761a3e4b9fa13a619
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-03-12 22:13:02 +0800

    加入專案檔案。

---
+++ ITSA4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sH = 0, sM = 0, eH = 0, eM = 0;
+
+    cin >> sH >> sM;
+    cin >> eH >> eM;
+    int time = (eH * 60 + eM) - (sH * 60 + sM);
+    if (time <= 120 && time >= 0) {
+        cout << time / 30 * 30 << endl;
+    }
+    else if (time > 120 && time <= 240) {
+        cout << (time - 120) / 30 * 40 + 120 << endl;
+    }
+    else {
+        cout << (time - 240) / 30 * 60 + 120 + 160 << endl;
+    }
+    return 0;
+}
